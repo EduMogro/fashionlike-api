75986c16f6e505cf0f0751c1c82bb13814319d58
EduMogro <edumogro@outlook.com>
2022-04-04 05:43:47 -0300

    feat: add Posts

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +16,30 @@ use App\Http\Controllers\Api\UserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+// AUTH
+
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
 Route::group( ['middleware' => ["auth:sanctum"]], function() {
     Route::get('user-profile', [UserController::class, 'userProfile']);
     Route::get('logout', [UserController::class, 'logout']);
+    Route::get('usuario', [PostController::class, 'usuario']);
+    
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// POSTS
+
+Route::get('/posts', [PostController::class, 'index']);
+// Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{id}', [PostController::class, 'update']);
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+// });
